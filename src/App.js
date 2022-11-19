1f89915828e5ca421346069bfd53384b0bebdac2
aniiaham <aniya_axm@icloud.com>
2022-11-18 21:28:21 -0500

    need to finish

--- src/App.js
+++ src/App.js
@@ -1,11 +1,41 @@
-import './App.css';
+import "./App.css";
+import React, { useState, useEffect } from "react";
+import axios from "axios";
+
+const API_URL = "https://zoo-animal-api.herokuapp.com/animals/rand";
 
 function App() {
+  const [animalData, setAnimailData] = useState({});
+
+  useEffect(() => {
+    const fetchData = () => {
+      axios
+        .get("https://zoo-animal-api.herokuapp.com/animals/rand")
+        .then((res) => {
+          setAnimailData(res.data);
+        })
+        .catch((error) => {
+          // console.log(error)
+        });
+    };
+     fetchData();
+  }, []);
+
   return (
     <div className="App">
-      hello world
+      <DisplayAnimals animalData={animalData} />
     </div>
   );
 }
 
 export default App;
+
+const DisplayAnimals = (props) => {
+  const { animalData } = props;
+  console.log("THIIIIIIS", animalData);
+  return (
+    <div>
+        <div>{animalData.name}</div>
+    </div>
+  );
+};
